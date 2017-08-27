fixed degrees/radians issue. Changed up launch files a bit

scripts/radar_visualization.py
@@ -3,7 +3,7 @@ import sys
 import rospy
 import os
 
-from cacc_msgs.msg import RadarData
+from cacc_msgs.msg import RadarData,RangeEstimationOutput  
 from sensor_msgs.msg import CameraInfo,Image
 
 from cv_bridge import CvBridge, CvBridgeError
@@ -20,36 +20,49 @@ class RadarVizNode():
 
     self.bridge = CvBridge()
     
+    # --- Vizualization parameters
     self.boxWidth = rospy.get_param('~box_width',1.0)
+    self.lineWidth = rospy.get_param('~line_width',1)
+    self.showRangeEstimate = rospy.get_param('~show_range_estimate',True)
+
+    # --- Extrinsic parameters
     camPitchDeg = rospy.get_param('~camera_pitch_offset_deg',0.0)
     camYawDeg = rospy.get_param('~camera_yaw_offset_deg',0.0)
     self.camHeight = rospy.get_param('~camera_height_offset',2.0)
     self.camLongOffset = rospy.get_param('~camera_longitudinal_offset',2.0)
     self.camLatOffset = rospy.get_param('~camera_lateral_offset',0.0)
-    radarTopic = rospy.get_param('~radar_topic',"/delphi_node/radar_data")
-    imageTopic = rospy.get_param('~image_topic',"/axis_decompressed")
-    overlayTopic = rospy.get_param('~image_overlay_topic',"/radar_overlay")
-    
+
     self.camPitch = camPitchDeg*pi/180.0
     self.camYaw = camYawDeg*pi/180.0
 
-    radar_sub = rospy.Subscriber(radarTopic,RadarData,self.radarCallback,queue_size=1)
-    image_sub = rospy.Subscriber(imageTopic,Image,self.imageCallback,queue_size=1)
+    # --- Topic names
+    rangeTopic = rospy.get_param('~range_estimate_topic',"/range_kf_node/rangeEstimationOutput")
+    radarTopic = rospy.get_param('~radar_topic',"/delphi_node/radar_data")
+    imageTopic = rospy.get_param('~image_topic',"/axis_decompressed")
     
-    self.image_pub = rospy.Publisher(overlayTopic,Image,queue_size=1)
+    # --- ROS publisher/subscriber
+    self.image_pub = rospy.Publisher("~image_overlay",Image,queue_size=1)
 
+    rospy.Subscriber(radarTopic,RadarData,self.radarCallback,queue_size=1)
+    rospy.Subscriber(imageTopic,Image,self.imageCallback,queue_size=1)
+    
+    if self.showRangeEstimate:
+      rospy.Subscriber(rangeTopic,RangeEstimationOutput,self.rangeCallback,queue_size=1)
+    
+    # --- Class variables
     self.range = np.zeros( (64,1) ) # radar ranges
     self.angle = np.zeros( (64,1) ) # radar angles
+
+    self.estRange = None;
+    self.estAngle = None;
 
     self.Mint = np.matrix((  (941.087953,0.000000,624.481729),
                              (0.000000,942.665887,382.681338),
                              (0.000000,0.000000,1.000000)   ))
 
-    self.imageWidth = 0
-    self.imageHeight = 0
-
-    self.xpos = 1.0
-    self.ypos = -15.0
+  def rangeCallback(self,msg):
+    self.estRange = msg.range;
+    self.estAngle = msg.azimuth;
 
   def radarCallback(self,msg):
     for i in range(0,64):
@@ -61,52 +74,43 @@ class RadarVizNode():
 
 
   def imageCallback(self,msg):
-    # print "image - here"
     # --- Convert to OpenCV image format
     try:
       img = self.bridge.imgmsg_to_cv2(msg, "passthrough")
     except CvBridgeError as e:
       print(e)
 
-    shp = img.shape
-    self.imageHeight = shp[0]
-    self.imageWidth = shp[1]
-
-
-    ########### Example #############################
-    # self.xpos = (self.xpos + 2.5)%75.0
-
-    # r = pow(pow(self.xpos,2) + pow(self.ypos,2),0.5)
-    # ang = atan2(self.ypos,self.xpos)
-
-    # boxPoints = self.getBoxPoints(r,ang)
-
-    # self.boxImageOverlay(boxPoints,img)
-    #################################################
-
-    # Map angle to pixel location
+    # --- Overlay radar tracks
     for i in range(0,64):
       if (self.range[i]>0):
+        # Project range/angle to pixel location
         boxPoints = self.getBoxPoints(self.range[i],self.angle[i])
-        self.boxImageOverlay(boxPoints,img)
-        
+        self.boxImageOverlay(boxPoints,(0,0,255),img)
+    
+    # --- Overlay estimator solution
+    if self.estRange is not None:
+      boxPoints = self.getBoxPoints(self.estRange,self.estAngle)
+      # boxPoints = self.getBoxPoints(5.0,0.0)
+      print boxPoints
+      self.boxImageOverlay(boxPoints,(255,0,0),img)
+
+    # --- Convert CV format into ROS format
     img_out = img
 
-    # Convert CV format into ROS format
     image_msg = self.bridge.cv2_to_imgmsg(img_out, "bgr8")
 
     image_msg.header = msg.header
 
     self.image_pub.publish(image_msg)
 
-  def boxImageOverlay(self,boxPoints,img):
+  def boxImageOverlay(self,boxPoints,bgr,img):
     for pt1 in boxPoints.T:
         for pt2 in boxPoints.T:
             if pt1 is not pt2:
                 px1,py1 = self.getCameraProjection(pt1.T)
                 px2,py2 = self.getCameraProjection(pt2.T)
                 if ( (px1 is not None) and (px2 is not None) ):
-                    cv2.line(img,(px1,py1),(px2,py2),(0,0,255),1)
+                    cv2.line(img,(px1,py1),(px2,py2),bgr,self.lineWidth)
 
   def getBoxPoints(self,r,ang):
     r_sp_s = np.matrix( (r*cos(ang),r*sin(ang),0.0) ).T # rpv from radar ("sensor") to point resolved in the sensor frame
@@ -116,8 +120,8 @@ class RadarVizNode():
     dlt = self.boxWidth*0.5
 
     deltas = np.matrix(( (0.,0.,0.,0.),
-                         (1.,1.,-1.,-1.),
-                         (1.,-1.,-1.,1.)  ))
+                         (dlt,dlt,-dlt,-dlt),
+                         (dlt,-dlt,-dlt,dlt)  ))
 
     return (boxPoints + deltas)
 
@@ -141,9 +145,6 @@ class RadarVizNode():
 
     x_im = (x_[0]/x_[2]).astype(int)
     y_im = (x_[1]/x_[2]).astype(int)
-
-    # if ( (x_im<0) or (x_im>self.imageWidth) or (y_im<0) or (y_im>self.imageHeight) ):
-    #   return (None,None)
     
     return (x_im,y_im)
 